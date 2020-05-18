1a7c30be1e8e4e66564059368b90f87e75a02696
dewarsh <dewarshjoshi505@gmail.com>
2020-05-18 19:14:54 +0530

    Loading Handlebars module and setting the engine in index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 
 // app.get('/', (req, res) => {
 //     console.log('Demo on handling http requests for URL /api/members')
 // })
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Body parser because body of request Object was unable to read JSON
 app.use(express.json())
